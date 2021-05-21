facdf1f74c655571159cf52de162979c987cf2d2
Kacper <kacpergierycz@gmail.com>
2021-05-21 12:46:49 +0200

    UserDeatail Entity and save to Mysql

--- Hibernate2/src/main/java/AdvanceHibernate/Hibernate2/App.java
+++ Hibernate2/src/main/java/AdvanceHibernate/Hibernate2/App.java
@@ -1,5 +1,12 @@
 package AdvanceHibernate.Hibernate2;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +15,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+    	userDetailsDB();
+    	
+
+    	
     }
+    
+    public static void userDetailsDB(){
+    	
+    	UserDetails user = new UserDetails();
+    	user.setUserId(1);
+    	user.setUserName("Kacper");
+    	
+    	Configuration con = new Configuration().configure().addAnnotatedClass(UserDetails.class);    	
+    	ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();   	   	
+    	SessionFactory sf=con.buildSessionFactory(reg); 	
+    	Session session=sf.openSession();
+    	session.beginTransaction();
+    	
+    	
+    	session.save(user);
+    	
+    	session.getTransaction().commit();
+    	session.close();
+    	
+    }
+    
+    
 }
