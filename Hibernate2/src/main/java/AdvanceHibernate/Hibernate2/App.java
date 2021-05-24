7a7d16f727401d0f2f3380f5d6d73200b0edbf30
Kacper <kacpergierycz@gmail.com>
2021-05-24 10:43:44 +0200

    @ElementCollection @JoinTable(name="")

--- Hibernate2/src/main/java/AdvanceHibernate/Hibernate2/App.java
+++ Hibernate2/src/main/java/AdvanceHibernate/Hibernate2/App.java
@@ -18,9 +18,48 @@ public class App
     public static void main( String[] args )
     {
 
-    	userDetailsDB();
-    	
+    //	userDetailsDB();
+    	setOfAddresses();
 
+    	
+    }
+    
+    public static void setOfAddresses() {
+    	
+    	UserDetails user1 = new UserDetails();
+    	user1.setUserName("First User");
+    	
+    	Address addr= new Address();
+    	Address addr2= new Address();
+    	
+    	addr.setCity("Warsaw");
+    	addr.setState("Mazowsze");
+    	addr.setStreet("Siemiatycka");
+    	addr.setPincode("00122");
+    	
+    	addr2.setCity("Warsaw2");
+    	addr2.setState("Mazowsze3");
+    	addr2.setStreet("Siemiatycka4");
+    	addr2.setPincode("001222");
+    	
+    	user1.getListOfAddresses().add(addr);
+    	user1.getListOfAddresses().add(addr2);
+    	
+    	
+    	Configuration con = new Configuration().configure().addAnnotatedClass(UserDetails.class);    	
+    	ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();   	   	
+    	SessionFactory sf=con.buildSessionFactory(reg); 	
+    	Session session=sf.openSession();
+    	session.beginTransaction();
+    	
+    	
+    	session.save(user1);
+
+
+    	
+    	session.getTransaction().commit();
+    	session.close();
+    	
     	
     }
     
@@ -31,12 +70,21 @@ public class App
     	
     	
     	Address addr= new Address();
+    	Address addr2= new Address();
+
     	
     	addr.setCity("Warsaw");
     	addr.setState("Mazowsze");
     	addr.setStreet("Siemiatycka");
     	addr.setPincode("00122");
-    	user1.setAddress(addr);
+  //  	user1.setHomeAddress(addr);
+    	
+    	addr2.setCity("Warsaw2");
+    	addr2.setState("Mazowsze3");
+    	addr2.setStreet("Siemiatycka4");
+    	addr2.setPincode("001222");
+  //  	user1.setOfficeAddress(addr2);
+
     	
 //    	user.setAddress("FirstOneAddress");
 //    	user.setJoinDate(new Date());
--- Hibernate2/src/main/java/AdvanceHibernate/Hibernate2/UserDetails.java
+++ Hibernate2/src/main/java/AdvanceHibernate/Hibernate2/UserDetails.java
@@ -1,12 +1,19 @@
 package AdvanceHibernate.Hibernate2;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -19,23 +26,58 @@ public class UserDetails {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private int userId;
 	private String userName;
-	@Embedded
-	private Address address;
+	@ElementCollection
+	@JoinTable(name="USER_ADDRESS")
+	private Set<Address>listOfAddresses= new HashSet();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@Embedded
+//	@AttributeOverrides({
+//	@AttributeOverride(name="street", column=@Column(name="HOME_STREET_NAME")),
+//	@AttributeOverride(name="city", column=@Column(name="HOME_CITY_NAME")),
+//	@AttributeOverride(name="state", column=@Column(name="HOME_STATE_NAME")),
+//	@AttributeOverride(name="pincode", column=@Column(name="HOME_PINCODE_NAME"))
+//	})
+//	private Address homeAddress;
+//	@Embedded
+//	private Address officeAddress;
+	
+	
 //	@Temporal( TemporalType.DATE)
 //	private Date joinDate;
 //	private String Address;
 //	@Lob
 //	private String description;
 
-	public Address getAddress() {
-		return address;
+
+	public Set<Address> getListOfAddresses() {
+		return listOfAddresses;
 	}
-	public void setAddress(Address address) {
-		this.address = address;
+	public void setListOfAddresses(Set<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
 	}
 	public int getUserId() {
 		return userId;
 	}
+//	public Address getHomeAddress() {
+//		return homeAddress;
+//	}
+//	public void setHomeAddress(Address homeAddress) {
+//		this.homeAddress = homeAddress;
+//	}
+//	public Address getOfficeAddress() {
+//		return officeAddress;
+//	}
+//	public void setOfficeAddress(Address officeAddress) {
+//		this.officeAddress = officeAddress;
+//	}
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
